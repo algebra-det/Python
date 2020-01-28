b5a760e08a5864d70ed0e82a9c18d349c1fe6b60
Algebra <60046240+algebra-det@users.noreply.github.com>
2020-01-29 02:03:10 +0530

    Update numpy_5_matrices.py

--- numpy_5_matrices.py
+++ numpy_5_matrices.py
@@ -6,6 +6,7 @@ arr = array([
             ])
 
 m = matrix(arr)         # We have a function to define that it's a matrix
+
 print("M:-\n",m)
 """ The output of this will look like simple multiple dimensional array
     but we can do the matrix work with this"""
@@ -24,20 +25,27 @@ print("Maximum value element in N ",n.max())
     Python have it inbuilt, if we simply put * to multiply , python will know its for matrix
     and will do the heavy multiplication for us"""
 
-m1 = matrix('1,2,3,4 ; 5,6,7,8 ; 4,5,6,7')      # 3X4
-m2 = matrix('7,6,5,4 ; 8,7,6,5 ; 4,3,2,1')      # 3X4
+m1 = matrix('1,2,3 ; 6,7,8 ; 4,6,7')      # 3X3
+m2 = matrix('7,6,4 ; 7,6,5 ; 3,2,1')      # 3X3
 
-m3 = m1 * m2;
+m3 = m1 * m2
 print(m3)
+#OR
+m4 = m1 @ m2
+print(m4)
+#OR
+m5 = m1.dot(m2)
+print(m5)
+
+# if we use matrix instead of array , than multiplly will do the simple multiplication
+m6 = multiply(m1,m2)
+print(m6)
+
 
 """ If we haven't used the matrix() , then python will not know that we want to use
-    the matrix's multiplication format, and hence it should have done the simple multiplication"""
-
-
-
-
-
-
-
-
-
+    the matrix's multiplication format, and hence it should have done the simple multiplication
+    But for matrix multiplication/@/dot to work , the two matrix should be of same dimension
+    and size
+    
+    for further knowledge goto numpy_multiplication_Matrix.py
+"""
