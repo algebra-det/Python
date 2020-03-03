4bb57f501a7e4e24fcce512f3d01f575cdd112b6
algebra-det <algebra-det@users.noreply.github.com>
2020-03-04 00:20:49 +0530

    Using get_clone_matches from difflib

--- Interactive_Dictionary/Dictionary.py
+++ Interactive_Dictionary/Dictionary.py
@@ -1,13 +1,34 @@
-import json
+import json , difflib           # we can also use from difflib import get_close_matches
+
+#difflib is a library that is used to get get_close_matches() , SequenceMatches, etc
 
 data = json.load(open("data.json"))
 
+# TO check a word we gotta use :- data["rain"] , that's the syntax
+
 def translate(w):
-    if w in data:
+    w = w.lower()                    # To make the string all-lower case as in json file every thing is in lower string
+    if w in data:                    # To check if the word is in json file or not
         return data[w]
-    else:
-        return "The word doesn't exist. Please double check it."
 
-word = input("Enter the word you want to search : ")
+    elif len(difflib.get_close_matches(w, data.keys())) > 0:        # Check if there's a match for the word
+        close = difflib.get_close_matches(w, data.keys())
+        taking = input("Did you mean %s instead ? \n Enter Y if yes, else press any key "%(close[0]))
 
-print(translate(word))
+        if taking.lower()=="y":             # if user inputs that the match is correct
+            return data[close[0]]
+        else:                               # if not correct
+            return "The word doesn't exist. Please double check it "
+
+    else:                            # If there's no such word or match
+        return "The word doesn't exist. Please double check it "
+
+word = input("Enter word : ")
+
+output = translate(word)
+
+if type(output)== list:
+    for item in output:
+        print(item)
+else:
+    print(output)
