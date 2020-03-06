a6426a0bdf2928b9df5319e4d1c7055697a68fd9
Algebra <60046240+algebra-det@users.noreply.github.com>
2020-03-06 23:22:40 +0530

    Create webmap4_GeoJson_Layer.py
    Adding a GeoJson Polygon Layer to the map using a .json file containing the data

---
+++ Webmap_with_folium/webmap4_GeoJson_Layer.py
@@ -0,0 +1,33 @@
+import folium
+import pandas
+import random
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation<1500:
+        return "green"
+    elif 1500<=elevation<2500:
+        return "grey"
+    else:
+        return "cadetblue"
+
+map = folium.Map(location=[39.828492, -100.542930], zoom_start=6, titles = "Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+
+# Using loop for more markers
+for lt, ln, el in zip(lat, lon,elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+"m",
+    fill_color=color_producer(el), color="grey", fill_opacity=0.7))
+
+# using GeoJason() in folium to take .json file
+fg.add_child(folium.GeoJson(data=(open("world.json","r", encoding="utf-8-sig").read())))
+
+map.add_child(fg)       # or fg.add_to(Map)
+
+map.save("Map1.html")
