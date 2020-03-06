67d0e582fa90136a53e7c5657daabd62159ff9fb
Algebra <60046240+algebra-det@users.noreply.github.com>
2020-03-06 22:51:03 +0530

    Create webmap2_Circles.py

---
+++ Webmap_with_folium/webmap2_Circles.py
@@ -0,0 +1,36 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+col = ['pink', 'lightgreen', 'blue', 'darkpurple', 'white', 'gray', 'orange', 'green', 'darkgreen', 'darkred', 'darkblue',
+    'lightgray', 'purple', 'black', 'cadetblue', 'red', 'lightblue', 'lightred', 'beige']
+
+# Creating a function that will return color according to the elevation value passed
+def color_producer(elevation):
+    if elevation<1000:
+        return 'green'
+    elif 1000<=elevation<2000:
+        return 'orange'
+    else:
+        return 'cadetblue'
+
+map = folium.Map(location=[39.828492, -100.542930], zoom_start=6, titles = "Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+
+# Using loop for more markers
+for lt, ln, el in zip(lat, lon,elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+"m",
+    fill_color=color_producer(el), color="grey", fill_opacity=0.7))
+
+map.add_child(fg)       # or fg.add_to(Map)
+
+map.add_child(folium.ClickForMarker(
+    popup="Waypoint"))
+
+map.save("Map1.html")
