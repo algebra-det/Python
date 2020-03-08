e64029a5c7abf930784dee480fc2a091d66c6689
Algebra <60046240+algebra-det@users.noreply.github.com>
2020-03-08 23:12:06 +0530

    Traversals
    Binary Tree Traversals i.e. In-Order traversal , Pre-Order traversal, Post-Order traversal.

---
+++ DataStructures/Binary_Tree_traversal.py
@@ -0,0 +1,56 @@
+class Node:
+	def __init__(self, key):
+		self.val = key
+		self.left = None
+		self.right = None
+
+def print_Inorder(root):
+
+	if root:
+
+		print_Inorder(root.left)
+
+		print(root.val, " - ",end="")
+
+		print_Inorder(root.right)
+
+def print_Preorder(root):
+
+	if root:
+
+		print(root.val, " - ",end="")
+
+		print_Preorder(root.left)
+
+		print_Preorder(root.right)
+
+def print_Postorder(root):
+
+	if root:
+
+		print_Postorder(root.left)
+
+		print_Preorder(root.right)
+
+		print(root.val, " - ",end="")
+
+#		1
+#	   / \
+#	  2   3
+#	 / \
+#	4   5
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Pre-Order Traversal of binary tree is : ")
+print_Inorder(root)
+print()
+print_Preorder(root)
+print()
+print_Postorder(root)
+print()
